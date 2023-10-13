a9f37647c090afc65e93f58997b6dc27eab06508
kavyapandey <kavyapandeyksp11@gmail.com>
2023-10-13 09:29:14 +0530

    Initial commit

---
+++ bin/practice_dartt.dart
@@ -0,0 +1,5 @@
+import 'package:practice_dartt/practice_dartt.dart' as practice_dartt;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${practice_dartt.calculate()}!');
+}
---
+++ lib/practice_dartt.dart
@@ -0,0 +1,6 @@
+void main(){
+  print(" Hello World");
+  print(4);
+  print (12/4);
+  print (false);
+}
