3b45236496e89eca39bde1f1bf4d74f80425adb2
kavyapandey <kavyapandeyksp11@gmail.com>
2023-10-15 19:53:18 +0530

    Stringg

---
+++ lib/Strings_Dart_Literals_and_String_Interpolationn.dart
@@ -0,0 +1,20 @@
+void main() {
+  //Literals
+  var isCool = true;
+  int x =2;
+  2;
+  "John";
+  4.5;
+
+  // Various ways to define String Literals in Dart
+  String s1 = 'Single';
+  String s2 = 'Double';
+  String s3 = 'It\'s easy';
+  String s4 = "It's easy ";
+
+  String s5 = 'This is going to be very long String.'
+      ' This is just a sample String demo in Dart Programming language';
+
+  //String Interpolation
+  String name = "kevin";
+}
---
+++ lib/practicee_variablee_dataType.dart
@@ -0,0 +1,29 @@
+void main() { 
+  
+  //Numbers: int
+  int score = 23;
+  var count = 23;// it is inferred as integer automatically by compiler
+
+    int hexValue = 0xEADEBAEE;
+  
+  //Numbers: double
+  double percentage = 93.4;
+  var percent = 82.5333;
+  double exponents = 1.42e5;
+  
+  //Strings
+  String name = "Henry";
+  var company = "Google";
+
+  
+  //Boolean 
+  bool isValid ;
+  var isAlive = false;
+  
+  print(name);
+  print(exponents);
+
+  //NOTE: All data types in Dart are Objects.
+  // Therefore, their initial value is by default 'null
+  
+}
