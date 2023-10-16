3909764d04d8c744ba16d46cc5ab0b33bf585fb9
kavyapandey <kavyapandeyksp11@gmail.com>
2023-10-16 17:40:13 +0530

    final and const

--- lib/Strings_Dart_Literals_and_String_Interpolationn.dart
+++ lib/Strings_Dart_Literals_and_String_Interpolationn.dart
@@ -1,3 +1,6 @@
+// Strings,Literals and String Interpolation
+
+
 void main() {
   //Literals
   var isCool = true;
@@ -15,6 +18,21 @@ void main() {
   String s5 = 'This is going to be very long String.'
       ' This is just a sample String demo in Dart Programming language';
 
-  //String Interpolation
+  //String Interpolation => Use ["My name is $name"] instead of ["My name is" + name]
+  // not only for the string its also for the
   String name = "kevin";
+
+
+  print ( " My name is $name ");//never use curly bracket here double, int, boolean value
+  print("The number of characters is String Kevin is ${name.length}" );
+  
+  int l = 20;
+  int b = 10;
+  
+  print ("The sum of $l and $b is ${l + b}");
+  print("The are of rectangle with length is $l and breath is $b is ${l * b} ");
+
+
+
+
 }
---
+++ lib/finall_and_constt.dart
@@ -0,0 +1,24 @@
+void main() {
+
+  //final keyword
+  final cityName = 'Mumbai';
+
+  // cityName ='Delhi';
+
+  final String countryName = 'India';
+
+
+  // const keyword
+  const PI = 3.14;
+  const double gravity = 9.8;
+
+}
+
+class Circle {
+
+  final color = 'Red';
+  static const PI = 3.14;
+}
+
+
+// you can define final not const in class
