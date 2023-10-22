49125c6c423788412ec243ecd4860b0ed4506c58
kavyapandey <kavyapandeyksp11@gmail.com>
2023-10-22 15:40:52 +0530

    Conditional Expressionn

---
+++ lib/Switchh_and_Casee.dart
@@ -0,0 +1,23 @@
+void main() {
+// Switch Case Statements : Applicable for only 'int' and 'String'
+  String grade = 'A';
+    //switch boolean ,double not use
+  switch (grade) {
+    case 'A' :
+      print("Excellent grade of A");
+      break;
+    case 'B' :
+      print("Very Good !");
+      break;
+    case 'C' :
+      print(" Good enough . But work hard");
+      break;
+    case 'F' :
+      print(" You have failed");
+      break;
+
+    default :
+      print("Invalid Grade");
+
+  }
+}
