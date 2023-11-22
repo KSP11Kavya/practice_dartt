b148a3865ec86e9169e222f365c8f283769cf4f6
kavyapandey <kavyapandeyksp11@gmail.com>
2023-11-22 13:40:13 +0530

    print & std

--- lib/practice_dartt.dart
+++ lib/practice_dartt.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+//it come when u want to take input & out output
+
 void main(){
-  print(" Hello World");
-  print(4);
-  print (12/4);
-  print (false);
+//void mean empty it doesnt retuen anything in this other func. return type come
+
+print('Welcome to Dart!');
+stdout.write('Enter your Name');
+
+var name = stdin.readLineSync();
+print("Welcome, $name");
+//stdin
 }
+//u can use print or stdut for print anything
+//concel value output/input
