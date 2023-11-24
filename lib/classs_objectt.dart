4d974e6fbc6d7e133e41129ab727ca1847e08584
kavyapandey <kavyapandeyksp11@gmail.com>
2023-11-24 18:38:39 +0530

    print & std

---
+++ lib/classs_objectt.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main(){
+
+
+
+  print(" Welcome to Dart!");
+
+  stdout.write('Enter your Name: ');
+
+  var name = stdin.readLineSync();
+
+  print(" Welcome, $name ");
+
+  Human(); // creating a Class Object
+
+}
+
+class Human {
+  //curly baces is scope resoulation
+  //class is a keyword
+  //Human is class what i create
+  Human();
+}
---
+++ lib/variablee_dataTypee.dart
@@ -0,0 +1,49 @@
+import 'dart:io';
+
+void main(){
+
+
+
+  print(" Welcome to Dart!");
+
+  // stdout.write('Enter your Name: ');
+  //
+  // var name = stdin.readLineSync();
+  //
+  // print(" Welcome, $name ");
+  //
+  // Human(); // creating a Class Object
+
+  //Declaration of Variable
+  int a ;
+//if u dont assigne any value  then it can be nullable so for making non nullavle be sure to assign the value
+//value can override in variable
+  //Assign
+  //= symbol is  assign
+  //in this right hand signed value added to left hand side
+  a= 5;
+  print ( a);
+  a = 7;
+  print(a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+class Human {
+  //curly baces is scope resoulation
+  //class is a keyword
+  //Human is class what i create
+  Human();
+}
