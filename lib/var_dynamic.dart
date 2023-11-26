773d05afce6713b2b296bbed96dda6981ec4fcb9
kavyapandey <kavyapandeyksp11@gmail.com>
2023-11-26 12:45:02 +0530

    var vs dynamaic

---
+++ lib/var_dynamic.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  print(" Welcome to Dart!");
+
+  String a = " Raman ";
+
+  //var
+  dynamic section;
+// var ma data store nhi kiya voh automatically dynamic bnjayaga
+  //var section;
+  //while intizalizing data type you
+  // dont inizalized any value it will bw dynamic
+  //You can assign any value with taking dyanimc type of data
+  section = "D"; //String
+
+  section = 7; //Int
+
+  section = false; //Bool
+
+  var rollno = 11;
+  //in var you can string if it is string
+  rollno = 16;
+}
