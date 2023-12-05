75fa957012355ec422704c0d72ba1ae02c781a27
kavyapandey <kavyapandeyksp11@gmail.com>
2023-12-05 15:42:34 +0530

    functionn

---
+++ lib/functionn.dart
@@ -0,0 +1,73 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  print(" Welcome to Dart!");
+
+  String a = " Raman ";
+
+  //var
+  dynamic section;
+
+  section = "D"; //String
+
+  section = 7; //Int
+
+  section = false; //Bool
+
+  var rollno = 11;
+  //in var you can string if it is string
+  rollno = 16;
+}
+// //Function or method are same
+// //function main role is to reduce the redundancy of the
+// //increase reusability in the class we use function
+// import 'dart:io';
+// import 'dart:math';
+// void main() {
+//   print(" Welcome to Dart!");
+//   var myC = myClass();
+//   myC.printName("kdd");// Func. Calling
+//   //
+//   //
+//   //
+//   //
+//   myC.printName("mhl");
+//   //
+//
+//   //
+//   //
+//   //
+//   myC.printName("Ramn");
+//   //
+//   //
+//   //
+//   //
+//   //
+//   //
+//
+//   myC.printName("Ramanujan");
+//
+//
+// }
+// class myClass{
+//   void printName(String name){//Declaration
+//     print(name);//Defination
+//   }
+// }
+//value already defined
+// void main() {
+//   print(" Welcome to Dart!");
+//   var myC = myClass();
+//   myC.printName();// Func. Calling
+//
+// }
+// class myClass{
+//   void printName(){//Declaration
+//     print('ksp');//Defination
+//   }
+// }
+
+// void myFunc() {
+//
+// }
