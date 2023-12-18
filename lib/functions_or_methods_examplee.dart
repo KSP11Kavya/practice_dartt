23e12e9c870e1a1325b24ff14f4b67e9319f964d
kavyapandey <kavyapandeyksp11@gmail.com>
2023-12-18 12:30:28 +0530

    all done

---
+++ lib/functions_or_methods_examplee.dart
@@ -0,0 +1,16 @@
+//OBJECTIVES
+//1. Define a Function
+//2. Pass parameters to a Function
+//3. Return value from a Function
+//4. Test that by default a Function return null
+void main (){
+
+  findPerimeter();
+}
+void findPerimeter() {
+  int length = 4;
+  int breadth = 2;
+
+  int perimeter = 2 * ( length + breadth);
+  print("The perimeter is $perimeter ");
+}
