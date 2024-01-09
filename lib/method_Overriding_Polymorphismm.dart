6e0b99db2293c2ee2726b711e6a5feb159f5844e
kavyapandey <kavyapandeyksp11@gmail.com>
2024-01-09 13:18:03 +0530

    all done

--- lib/method_Overriding_Polymorphismm.dart
+++ lib/method_Overriding_Polymorphismm.dart
@@ -2,9 +2,12 @@
 //1. Exploring Method Overriding
 void main() {
 
+  var dog = Dog();
+  dog.eat();
+  print(dog.color);
 }
 class Animal {
-  String? color;
+  String color = "brown";
   void eat(){
     print("Animal is eating");
   }
@@ -12,7 +15,17 @@ class Animal {
 
 class Dog extends Animal {
   String ? breed;
+  String color = "black";
   void bark() {
     print("Bark !");
   }
+  void eat (){
+    print("Dog is eating !");
+    super.eat();
+    print("More food to eat");
+
+  }
 }
+//when the stmt is executed the compiler give more proprite to the
+// instance variable of color
+//within the class of dog.
