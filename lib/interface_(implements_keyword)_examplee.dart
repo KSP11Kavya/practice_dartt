96de7bf6b5ed2bb9e794ef0d4d2d7c1081968e8d
kavyapandey <kavyapandeyksp11@gmail.com>
2024-01-09 16:20:05 +0530

    all done

--- lib/interface_(implements_keyword)_examplee.dart
+++ lib/interface_(implements_keyword)_examplee.dart
@@ -1,41 +1,41 @@
-
 //Objectives
 //1. Interface
 
-void main(){
-
+void main() {
   var tv = Television();
   tv.volumeUp();
   tv.volumeDown();
 }
-class Remote{
-  void volumeUp(){
-    print("_Volume Up from Remote_");
 
+class Remote {
+  void volumeUp() {
+    print("_Volume Up from Remote_");
   }
-  void volumeDown(){
+
+  void volumeDown() {
     print(" ___Volume Down from Remote___");
   }
 }
-class AnotherClass{
-  void justAnotherMethod(){
+
+class AnotherClass {
+  void justAnotherMethod() {
     //Code
   }
 }
+
 //Here Remote acts as Interface
-class Television implements Remote, AnotherClass{
+class Television implements Remote, AnotherClass {
   //Because of implements u can use more than one class but if u use extend u can only use one class
 
-
   void volumeUp() {
-   print("____Volume Up in Television______");
+    print("____Volume Up in Television______");
   }
 
   void volumeDown() {
     print("____Volume Down in Television______");
   }
-  void justAnotherMethod(){
-   print("Some code");
-  }
 
+  void justAnotherMethod() {
+    print("Some code");
+  }
 }
